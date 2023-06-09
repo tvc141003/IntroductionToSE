525705cc23b7d080fbbc4dc531d7fdc47ec293f9
trong <lehuutrong141@gmail.com>
2023-06-09 22:37:38 +0700

    update

--- IntroduceToSE/src/main/java/HCMUS/IntroduceToSE/App.java
+++ IntroduceToSE/src/main/java/HCMUS/IntroduceToSE/App.java
@@ -1,5 +1,8 @@
 package HCMUS.IntroduceToSE;
 
+
+
+
 /**
  * Hello world!
  *
@@ -8,6 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    
     }
 }
--- IntroduceToSE/src/main/java/main/Main.java
+++ IntroduceToSE/src/main/java/main/Main.java
@@ -20,22 +20,13 @@ public class Main {
 		
 		Transaction tx = session.beginTransaction();
 		
-		Student std = new Student("STU05", "Le", "Nguyen", true);
-		
-<<<<<<< HEAD
+
 		MailUtils mailUtils = MailUtils.getInstance();
 		
 		mailUtils.sendEmail("lehuutrong141@gmail.com", "hello world");
 		
 	
-=======
-		Subject sub = new Subject("SUB09", "Machine learning", 4);
-		
-		session.save(std);
-		session.save(sub);
->>>>>>> ca5e81dafc0c0f8dd469c0f97f71e77ab0eedda2
-		
-		
+
 		try {
 			tx.commit();
 		} 
