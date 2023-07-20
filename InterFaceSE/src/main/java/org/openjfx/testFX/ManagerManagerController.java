257d055c9341d90823f608cc54bfa5a82ddb8781
hung <hung8110999@gmail.com>
2023-07-20 22:41:39 +0700

    add manager/subject controller

---
+++ InterFaceSE/src/main/java/org/openjfx/testFX/ManagerManagerController.java
@@ -0,0 +1,45 @@
+package org.openjfx.testFX;
+
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+public class ManagerManagerController {
+
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    @FXML
+    public void onClickToAddManager(ActionEvent event)throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("ManagerManagerAdd.fxml"));
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.show();
+    }
+    @FXML
+    public void onClickToUpdateManager(ActionEvent event)throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("ManagerManagerUpdate.fxml"));
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.show();
+    }
+    @FXML
+    public void onClickToBackHomeManager(ActionEvent event)throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("menuHomeManager.fxml"));
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.show();
+    }
+}
+
---
+++ InterFaceSE/src/main/java/org/openjfx/testFX/ManagerSubjectAddController.java
@@ -0,0 +1,28 @@
+package org.openjfx.testFX;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+public class ManagerSubjectAddController {
+
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    @FXML
+    public void onClickToBackManagerStudent(ActionEvent event)throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("ManagerSubject.fxml"));
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.show();
+    }
+}
+
